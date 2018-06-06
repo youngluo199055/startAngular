e9a69762f0c1b09827cbef586deb4deb3a225e0a
youngluo199055 <luoyang19900505@outlook.com>
2018-06-06 11:26:08 +0800

    one
    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "../hero";
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Young Luo'
+  // };
+  heroes: Hero[];
+  // selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  /** POST: add a new hero to the server */
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+}
