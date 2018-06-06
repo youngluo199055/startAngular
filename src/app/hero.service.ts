51fffb5a6ca8dc37916a5a3885fc1dcdec278246
youngluo199055 <luoyang19900505@outlook.com>
2018-06-06 14:00:12 +0800

    1

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -90,7 +90,7 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  private heroesUrl = 'api/heroes'; // URL to web api
+  private heroesUrl = 'api/heroes'; // URL to web api 
 
 
   /**
